692b299ffd35b7b8f7e62686d22096fbb13efcd5
wfrotj <trebrodrigo@gmail.com>
2023-08-13 21:04:36 +0800

    fix error

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import config from "./utils/config.js";
 import mongoose from "mongoose";
 import messageRouter from "./routes/messageRouter.js";
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
 
 const connectToDB = async (url) => {
@@ -11,10 +13,21 @@ const connectToDB = async (url) => {
 };
 
 connectToDB(config.MONGODB);
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 app.use("/", messageRouter);
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 export default app;
